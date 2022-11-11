4bd4899df33352a8984e46dfc4634efe18b75bac
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-11-11 21:56:07 +0300

    оформление

--- src/main/java/org/etalonnoeReshenie/Main.java
+++ src/main/java/org/etalonnoeReshenie/Main.java
@@ -2,19 +2,26 @@ package org.etalonnoeReshenie;
 
 import static org.etalonnoeReshenie.CityUtils.*;  // статический импорт для более читабельного кода
 import static org.etalonnoeReshenie.CitiesSorting.*;
+import static org.etalonnoeReshenie.IndexCityFinder.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
+/*
 
-//        printer(citiesCollector());
+       //task # 1
+        printer(citiesCollector());
 
+
+        //task # 2
         printSortingCity(citiesCollector(), new nameComparator()); // по имени города
-
         printSortingCity(citiesCollector(), new districtAndNameComparator());  // по округам и именам внутри округов
+        lambdaSortedByName(citiesCollector());  // реализация lambda выражения
+        lambdaSortedByDistricts(citiesCollector());  // реализация lambda выражения
 
-//        lambdaSortedByName(citiesCollector());  // реализация lambda выражения
-
-//        lambdaSortedByDistricts(citiesCollector());  // реализация lambda выражения
+*/
+        // task # 3
+        converter(citiesCollector());
     }
 }
